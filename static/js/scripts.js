31075713ac902ece1d4131ed1238481dc2f47c68
Isabelle Goldstein <isabelle.s.goldstein@gmail.com>
2025-01-10 14:54:10 -0500

    Update scripts.js
    change line 5: 
    const section_names = ['home', 'publications', 'artsec'] //,'awards']
    
    to : 
    const section_names = ['home', 'publications', 'artsection'] //,'awards']

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'artsec'] //,'awards']
+const section_names = ['home', 'publications', 'artsection'] //,'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
