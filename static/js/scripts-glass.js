51c9b052cd7cecbbc54375a2726dc28df532d3f4
Isabelle Goldstein <isabelle.s.goldstein@gmail.com>
2025-01-10 14:47:05 -0500

    Update scripts-glass.js

--- static/js/scripts-glass.js
+++ static/js/scripts-glass.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['arthome','glassart']
+const section_names = ['arthome','artsec']
 
 
 window.addEventListener('DOMContentLoaded', event => {
