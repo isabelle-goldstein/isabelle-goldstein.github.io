09c400a245cf2401629f538aaf16561cc2772c6d
Isabelle Goldstein <isabelle.s.goldstein@gmail.com>
2025-01-11 15:45:06 -0500

    Update scripts-glass.js
    change section name from artsec to artsection

--- static/js/scripts-glass.js
+++ static/js/scripts-glass.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['arthome','artsec']
+const section_names = ['arthome','artsection']
 
 
 window.addEventListener('DOMContentLoaded', event => {
